015774221274a70bb46fda1a1313f76b397d6da0
Robin Kolk <robinkolk@msn.com>
2019-09-25 22:58:12 +0200

    this time really fixed the refresh timout when no connection can be established

--- custom_components/MasterVoltXS/sensor.py
+++ custom_components/MasterVoltXS/sensor.py
@@ -134,11 +134,8 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
           s.connect((TCP_IP, TCP_PORT)) # connect to tcp-converter
       except:
          _LOGGER.error("Unable to connect to tcp converter" )
-         _LOGGER.error("Unable to connect to tcp converter" )
-          # quit
-          
-          # throttle reconnect attempts
-         #await asyncio.sleep(reconnectInterval,loop=hass.loop)
+         await asyncio.sleep(reconnectInterval,loop=hass.loop)
+          # quit & throttle reconnect attempts
       else:
           # "flush":
           while (1):
@@ -257,5 +254,3 @@ class SunMasterXSSensor(Entity):
     def icon(self):
         """Return the icon to use in the frontend, if any."""
         return ICON
-
-
