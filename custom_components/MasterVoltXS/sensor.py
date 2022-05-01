87cfe14cd8c7dabd195e5f96ee4c7a88e4951ca8
Robin Kolk <robinkolk@msn.com>
2022-05-01 18:55:32 +0200

    fixing issues with value tanking

--- custom_components/MasterVoltXS/sensor.py
+++ custom_components/MasterVoltXS/sensor.py
@@ -115,7 +115,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
                 device._state = data['aci']
             elif(device._name == 'PAc_Solar'):
                 device._state = data['acp']
-            elif(device._name == 'Wtot_Solar') and (data['totalpower'] > 0):
+            elif(device._name == 'Wtot_Solar') and (data['totalpower'] > 0) and (device._state < data['totalpower']):
                 device._state = data['totalpower']
             elif(device._name == 'Temp_Solar'):
                 device._state = data['temp']
